f62e533d4ca3e5a83a9f2a65ec2dd1c825803601
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-04-19 19:50:02 +0530

    peer setup done

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,4 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react"
-
 export default function Home() {
-  const socket = useSocket()
-
-  useEffect(() => {
-    socket?.on("connect", () => {
-      console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
-    });
-  }, [socket])
 
   return (
     <h1>Welcome</h1>
